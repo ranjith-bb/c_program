339ffc9a09881294eca8f7eb936815f408155faf
ranjith-bb <ranjithbb282005@gmail.com>
2024-07-21 19:46:12 +0530

    Create Check Prime in Range.c

---
+++ Check Prime in Range.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int start, end;
+
+    printf("Enter range (start end): ");
+    scanf("%d %d", &start, &end);
+
+    printf("Prime numbers between %d and %d are:\n", start, end);
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
